bef46e646f9d51d135a3d6f499ec3afadb87bc9f
Javiera Scarlette Angulo Barraza <javiera.angulo2005@gmail.com>
2026-01-21 02:33:57 -0300

    Estable 21-01

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import {
     collection,
     onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyA9YDOWM6ZKCriOECDgNeaZ_jXR9otmhNw",
     authDomain: "yoonnie-handmade.firebaseapp.com",
@@ -13,6 +14,7 @@ const firebaseConfig = {
     messagingSenderId: "1001154851705",
     appId: "1:1001154851705:web:27e9b33bb51a38792435a8"
 };
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const productosRef = collection(db, "productos");
@@ -31,20 +33,27 @@ onSnapshot(productosRef, (snapshot) => {
     `;
     });
 });
+
 onSnapshot(productosRef, (snapshot) => {
     contenedor.innerHTML = "";
+
     snapshot.forEach((doc) => {
         const producto = doc.data();
+        const id = doc.id; // Obtenemos el ID del producto
         contenedor.innerHTML += `
         <div class="col-12 col-sm-6 col-lg-3">
-            <div class="card h-100 shadow-sm">
-                <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}" style="height: 250px; object-fit: cover;">
+            <div class="card h-100 shadow-sm border-0">
+                <a href="producto.html?id=${id}">
+                    <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}" style="height: 250px; object-fit: cover; cursor: pointer;">
+                </a>
                 <div class="card-body d-flex flex-column text-center">
                     <h5 class="card-title fw-bold">${producto.nombre}</h5>
                     <p class="card-text text-primary fs-5 fw-bold">$${producto.precio}</p>
-                    <button class="btn btn-primary rounded-pill mt-auto w-100">
-                        Agregar al carrito
-                    </button>
+                    <div class="mt-auto">
+                        <a href="producto.html?id=${id}" class="btn btn-outline-primary rounded-pill w-100">
+                            Ver detalles 👁️
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
